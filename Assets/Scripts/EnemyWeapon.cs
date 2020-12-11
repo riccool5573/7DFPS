3c785a9e0f2e491b10e8cd3b636fb3f5852e1405
TheExiledCat <kazekage2002@hotmail.com>
2020-12-11 20:52:41 +0100

    Update EnemyWeapon.cs

--- Assets/Scripts/EnemyWeapon.cs
+++ Assets/Scripts/EnemyWeapon.cs
@@ -6,6 +6,7 @@ public class EnemyWeapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
     Transform barrel;
+    [SerializeField] float bulletVelocity;
     private void Start()
     {
         barrel = transform.GetChild(2);
@@ -14,7 +15,7 @@ public class EnemyWeapon : MonoBehaviour
     public void Shoot()
     {
         GameObject b= Instantiate(bulletPrefab, barrel.position, Quaternion.identity);
-        b.GetComponent<ConstantVelocity>().SetVelocity(transform.forward * Random.Range(100,200)/1000);
+        b.GetComponent<ConstantVelocity>().SetVelocity(transform.forward * bulletVelocity/100);
         Destroy(b, 5);
     }
 }
