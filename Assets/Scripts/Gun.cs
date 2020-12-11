1e9543398b64d5709abcff8133261e86773341a5
TheExiledCat <kazekage2002@hotmail.com>
2020-12-11 22:57:52 +0100

    added shit

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -44,6 +44,7 @@ public class Gun : ControllerInput
         // instantiate a bullethole, set make sure the player doesn't shoot every frame, start the recoil, and play the gunsound. then allow the player to shoot again.
         GameObject bullet = Instantiate(bulletPrefab, barrel.position, transform.rotation);
         bullet.GetComponent<ConstantVelocity>().SetVelocity(transform.forward * bulletVelocity / 100);
+        Destroy(bullet, 4f);
         canShoot = false;
         gun.Play();
         yield return new WaitForSeconds(0.2f);
