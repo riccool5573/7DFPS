1b2f8a6b4b7203628c749e105695bc99e25329e6
TheExiledCat <kazekage2002@hotmail.com>
2020-12-13 19:45:16 +0100

    fixed enemy problem and added lightning

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -8,17 +8,18 @@ public class FieldOfView : MonoBehaviour
     public float visionRange;
     [SerializeField] LayerMask Obstacles;
     public event Action OnSpot;
-    bool vision;
-    bool obstructed;
+    [SerializeField]bool vision;
+    [SerializeField]bool obstructed;
     Ray rayR;
     Ray rayL;
+    [HideInInspector]
     public Player p;
     public bool active = true;
     // Start is called before the first frame update
 
     private void Start()
     {
-        p = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        p = GameObject.FindObjectOfType<Player>();
     }
     // Update is called once per frame
     void Update()
