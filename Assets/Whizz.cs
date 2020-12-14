5939b522159b0760b7ee076b415bcb4d46f1b367
TheExiledCat <kazekage2002@hotmail.com>
2020-12-14 20:53:02 +0100

    added gun whizzes

---
+++ Assets/Whizz.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whizz : MonoBehaviour
+{
+    AudioSource aS;
+    AudioClip[] clips;
+    private void Start()
+    {
+        aS = GetComponent<AudioSource>();
+        clips = Resources.LoadAll<AudioClip>("Sounds/Effects");
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 1.5f)
+        {
+            aS.PlayOneShot(clips[Mathf.FloorToInt(Random.Range(1, 10))]);
+        }
+    }
+}
