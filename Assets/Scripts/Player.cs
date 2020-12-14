7e4f6d92de1c2448ba7a8c8bc9d619759ffffc47
TheExiledCat <kazekage2002@hotmail.com>
2020-12-14 14:23:35 +0100

    Return back to fixed version

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("EnemyBullet"))
