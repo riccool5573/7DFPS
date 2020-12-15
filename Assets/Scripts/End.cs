11b6b783e92ddc3d2d6b2bd8afa01063a3e7f67e
riccool5573 <26188@ma-web.nl>
2020-12-15 14:33:25 +0100

    cleanup and finishing up scripting

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
